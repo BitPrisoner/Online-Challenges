fde4222486ac9096e4d3b49983d3cabf1169999d
Andrew <aokr52@mail.umsl.edu>
2017-11-05 20:53:53 -0600

    Adding another challenge to work on.

---
+++ Easy/Challenge 338 [Easy]/cmake-build-debug/CMakeFiles/Challenge_338__Easy_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Challenge_338__Easy_.dir/main.cpp.obj"
+  "Challenge_338__Easy_.pdb"
+  "Challenge_338__Easy_.exe"
+  "Challenge_338__Easy_.exe.manifest"
+  "libChallenge_338__Easy_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Challenge_338__Easy_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Easy/Challenge 338 [Easy]/cmake-build-debug/CMakeFiles/Challenge_338__Easy_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Challenge_338__Easy_.
+# This may be replaced when dependencies are built.
